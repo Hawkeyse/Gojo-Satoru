c08fd5444bb15e43932e82515186f1f667d271a7
Hawkeyse <96210116+Hawkeyse@users.noreply.github.com>
2022-05-18 14:19:29 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['233508968298']
+global.premium = ['233508968298']
+global.ownernomer = '233508968298'
+global.ownername = '🐦Timtech'
+global.botname = 'Tim-Tech'
+global.footer = '©CLR-BOT.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Nigeria, Ghana'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Timtech'
+global.author = '🐦Timtech'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
